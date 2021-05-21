0f792c84d1e4befb5af64d4ada01eefd2b37544e
Juan Pablo Pinillos <jppinillos2002@gmail.com>
2021-05-20 19:46:14 -0500

    Las variables

---
+++ 2nd_module/III-variables.py
@@ -0,0 +1,66 @@
+"""
+Este tema es súmamente importante, ya que se usará de aquí en adelante, tanto en Python
+como en otros lenguajes.
+
+En programación se usa álgebra. No, no te asustes.
+
+a = 7
+b = 3
+c = a + b
+
+Si sabes cuánto vale c, no tienes porqué preocuparte, por ahora :)
+
+En programación, siempre se usan las VARIABLES. Una variable es la forma en la cual
+podemos guardar nuestros datos. Una variable es una caja mágica donde guardas
+los datos que quieras.
+
+Las variables deben ser nombradas de manera correcta. Aunque puedes ponerle el nombre
+que quieras, no deben empezar con números, no deben contener espacios y no pueden ser
+palabras reservadas.
+
+NOTA:
+
+- Una palabra reservada es...una palabra (valga la redundancia) que ya está ocupada por Python.
+  Es decir, Python mismo necesita ese nombre para referirse a algo.
+  Por ejemplo, no puede llamar una variable print, porque ya se ocupa ese nombre.
+
+Los componentes de la variable son:
+- Nombre
+- Valor
+
+"""
+
+unaVariable = 'un dato muy largo y difícil de recordar'
+
+"""
+De esta manera, no es necesario escribir 'un dato muy largo y difícil de recordar'
+cada que se quiera hacer referencia a ese dato. Con decir 'unaVariable' está ok.
+
+Las variables nos sirven para:
+- Manipular datos más fácilmente
+- Recordar datos sin mayor dificultad. (No nosotros, la computadora).
+"""
+
+
+# ________________________________________________________________
+
+# A continuación ejemplos de variables. ¡Crea las tuyas!
+
+
+var = 123
+
+saludo = 'Hola!'
+
+resta = 9-3
+
+print = 'Ups, aquí puede haber un error'
+
+fehcaCumpleaños = '25-06'
+
+
+
+"""
+La variable 'resta' tendrá un valor de 6
+
+¿Por qué? Lo veremos en el próximo capítulo
+"""
